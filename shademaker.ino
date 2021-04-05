d375ff80f07a3a8ac8594ff1e53e86ab25727dc1
Jamie Gilgen <jamie@gilgen.ca>
2021-04-05 13:40:05 -0400

    Add forward declarations

--- shademaker.ino
+++ shademaker.ino
@@ -3,6 +3,48 @@
 #include <Fsm.h>
 #include <EEPROM.h>
 
+// Forward declarations
+boolean moveBlindsUpButtonActive();
+boolean moveBlindsDownButtonActive();
+boolean atTop(AccelStepper *, int stopPin);
+boolean atBottom(AccelStepper *stepper);
+boolean autoModeEnabled();
+boolean readyForAutoCmd();
+boolean isBrightOutside();
+void homeStepper(AccelStepper *stepper, int stopPin);
+void moveBlinds(long longPosition);
+void stopBlinds();
+void initializeBottomPosition();
+void initializePinIO();
+void disableSolenoid();
+void homeSteppers();
+void initializeSwitchHandlers();
+void initializeSteppers();
+void initializeFSMTransitions();
+void enableSolenoid();
+void disableSolenoid();
+void runAutoModeRoutines();
+void initializeStepper(AccelStepper *stepper);
+void onSetupMoveUpStateEnter();
+void onMoveUpStateExit();
+void onSetupMoveDownStateEnter();
+void onSetupMoveDownStateExit();
+void onMovingDownStateEnter();
+void onUpStateEnter();
+void onUpStateExit();
+void onDownStateEnter();
+void onDownStateExit();
+void onSetupStateEnter();
+void onSetupStateExit();
+void onSetupMoveUpStateExit();
+void onMovingDownStateExit();
+void onMovingUpstateEnter();
+void onMovingUpStateExit();
+void onMovingUpStateEnter();
+void onMidwayStateEnter();
+void onMidwayStateExit();
+
+
 #define DEBUG_MODE true
 
 // Pin 0 not used
@@ -21,7 +63,7 @@
 #define A_STOP_PIN  13 // spindle direction
 #define UP_PIN      A0 // abort
 #define DOWN_PIN    A1 // hold
-#define A_STOP_PIN  A2 // resume
+#define TEMPERATURE A2 // resume
 #define LIGHT       A3 // coolant enable
 // Pin A4 not used
 // Pin A5 not used
@@ -78,13 +120,118 @@ unsigned long lastMoveAt = 0;
 long bottomPosition;
 boolean inSetupMode = false;
 long positions[4];
-
+int loopCnt = 0;
 
 AccelStepper xstepper = AccelStepper(AccelStepper::DRIVER, X_STEP_PIN, DIRECTION);
 
 OneButton upSwitch = OneButton(UP_PIN, SWITCHES_ACTIVE_LOW, SWITCHES_USE_PULLUP);
 OneButton downSwitch = OneButton(DOWN_PIN, SWITCHES_ACTIVE_LOW, SWITCHES_USE_PULLUP);
 OneButton autoSwitch = OneButton(AUTO_MODE, SWITCHES_ACTIVE_LOW, SWITCHES_USE_PULLUP);
+
+// FSM states
+State upState(&onUpStateEnter, NULL, &onUpStateExit);
+State downState(&onDownStateEnter, NULL, &onDownStateExit);
+State setupState(&onSetupStateEnter, NULL, &onSetupStateExit);
+State setupMoveUpState(&onSetupMoveUpStateEnter, NULL, &onSetupMoveUpStateExit);
+State setupMoveDownState(&onSetupMoveDownStateEnter, NULL, &onSetupMoveDownStateExit);
+State movingDownState(&onMovingDownStateEnter, NULL, &onMovingDownStateExit);
+State movingUpState(&onMovingUpStateEnter, NULL, &onMovingUpStateExit);
+State midwayState(&onMidwayStateEnter, NULL, &onMidwayStateExit);
+
+// FSM object
+Fsm fsm(&upState);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Initializing\n");
+  initializeBottomPosition();
+  initializePinIO();
+  disableSolenoid();  
+  initializeSteppers();
+  homeSteppers();
+  initializeSwitchHandlers();
+  initializeFSMTransitions();
+  fsm.run_machine();
+  Serial.println("Done initialization");
+}
+
+
+void loop() {
+  if (xstepper.distanceToGo() == 0 || digitalRead(X_STOP_PIN) == LOW) {
+    if (atTop(&xstepper, X_STOP_PIN) && xstepper.currentPosition() >= xstepper.targetPosition()) {
+      //Serial.println("Triggering MOTORS_AT_TOP");
+      fsm.trigger(MOTORS_AT_TOP);  
+    } else if (!inSetupMode && atBottom(&xstepper)) {
+      //Serial.println("Triggering MOTORS_AT_BOTTOM");
+      fsm.trigger(MOTORS_AT_BOTTOM);
+    }
+  }
+
+  // Only tick periodically to speed up the run loop. This
+  // allows the motors to run a bit smoother
+  if (loopCnt % 50 == 0) {
+    if (!inSetupMode) {
+      runAutoModeRoutines();
+    }
+
+    // Tick the switches
+    upSwitch.tick();
+    downSwitch.tick();
+    autoSwitch.tick();
+  }
+
+
+  // Run the steppers
+  xstepper.run();
+  loopCnt++;
+}
+
+void initializePinIO() {
+  pinMode(DIRECTION, OUTPUT);
+  pinMode(X_STEP_PIN, OUTPUT);
+  pinMode(Y_STEP_PIN, OUTPUT);
+  pinMode(Z_STEP_PIN, OUTPUT);
+  pinMode(A_STEP_PIN, OUTPUT);
+  pinMode(X_STOP_PIN, INPUT_PULLUP);
+  pinMode(Y_STOP_PIN, INPUT_PULLUP);
+  pinMode(Z_STOP_PIN, INPUT_PULLUP);
+  pinMode(A_STOP_PIN, INPUT_PULLUP);
+  pinMode(ENABLE_PIN, OUTPUT);
+  pinMode(SOLENOID, OUTPUT);
+  pinMode(UP_PIN, INPUT_PULLUP);
+  pinMode(DOWN_PIN, INPUT_PULLUP);
+  pinMode(LIGHT, INPUT_PULLUP);
+  pinMode(AUTO_MODE, INPUT_PULLUP);
+}
+
+void initializeFSMTransitions() {
+  // Transitions in and out of setup
+  fsm.add_transition(&upState, &setupState, AUTO_DOUBLE_CLICK, NULL);
+  fsm.add_transition(&setupState, &downState, AUTO_DOUBLE_CLICK, NULL);
+
+  // Setup up/down control
+  fsm.add_transition(&setupState, &setupMoveUpState, UP_CLICK, NULL);
+  fsm.add_transition(&setupMoveUpState, &setupState, UP_CLICK, NULL);
+  fsm.add_transition(&setupState, &setupMoveDownState, DOWN_CLICK, NULL);
+  fsm.add_transition(&setupMoveDownState, &setupState, DOWN_CLICK, NULL);
+  fsm.add_transition(&setupMoveUpState, &setupState, MOTORS_AT_TOP, NULL);
+
+  // Up to down
+  fsm.add_transition(&upState, &movingDownState, DOWN_CLICK, NULL);
+  fsm.add_transition(&movingDownState, &midwayState, DOWN_CLICK, NULL);
+  fsm.add_transition(&movingDownState, &midwayState, UP_CLICK, NULL);
+  fsm.add_transition(&midwayState, &movingDownState, DOWN_CLICK, NULL);
+  fsm.add_transition(&midwayState, &movingUpState, UP_CLICK, NULL);
+  fsm.add_transition(&movingDownState, &downState, MOTORS_AT_BOTTOM, NULL);
+
+  // Down to up
+  fsm.add_transition(&downState, &movingUpState, UP_CLICK, NULL);
+  fsm.add_transition(&movingUpState, &midwayState, UP_CLICK, NULL);
+  fsm.add_transition(&midwayState, &movingUpState, UP_CLICK, NULL);
+  fsm.add_transition(&movingUpState, &midwayState, DOWN_CLICK, NULL);
+  fsm.add_transition(&midwayState, &movingDownState, DOWN_CLICK, NULL);
+  fsm.add_transition(&movingUpState, &upState, MOTORS_AT_TOP, NULL);
+}
 
 // FSM transition events
 void onUpStateEnter() {
@@ -181,65 +328,6 @@ void onMovingDownStateExit() {
   stopBlinds();
 }
 
-// FSM states
-State upState(onUpStateEnter, NULL, &onUpStateExit);
-State downState(onDownStateEnter, NULL, &onDownStateExit);
-State setupState(onSetupStateEnter, NULL, &onSetupStateExit);
-State setupMoveUpState(onSetupMoveUpStateEnter, NULL, &onSetupMoveUpStateExit);
-State setupMoveDownState(onSetupMoveDownStateEnter, NULL, &onSetupMoveDownStateExit);
-State movingDownState(onMovingDownStateEnter, NULL, &onMovingDownStateExit);
-State movingUpState(onMovingUpStateEnter, NULL, &onMovingUpStateExit);
-State midwayState(onMidwayStateEnter, NULL, &onMidwayStateExit);
-
-// FSM object
-Fsm fsm(&upState);
-
-void setup() {
-  Serial.begin(9600);
-  Serial.print("Initializing\n");
-  initializeBottomPosition();
-  initializePinIO();
-  disableSolenoid();  
-  initializeSteppers();
-  homeSteppers();
-  initializeSwitchHandlers();
-  initializeFSMTransitions();
-  fsm.run_machine();
-  Serial.println("Done initialization");
-}
-
-int loopCnt = 0;
-
-void loop() {
-  if (xstepper.distanceToGo() == 0 || digitalRead(X_STOP_PIN) == LOW) {
-    if (atTop(&xstepper, X_STOP_PIN) && xstepper.currentPosition() >= xstepper.targetPosition()) {
-      //Serial.println("Triggering MOTORS_AT_TOP");
-      fsm.trigger(MOTORS_AT_TOP);  
-    } else if (!inSetupMode && atBottom(&xstepper)) {
-      //Serial.println("Triggering MOTORS_AT_BOTTOM");
-      fsm.trigger(MOTORS_AT_BOTTOM);
-    }
-  }
-
-  // Only tick periodically to speed up the run loop. This
-  // allows the motors to run a bit smoother
-  if (loopCnt % 50 == 0) {
-    if (!inSetupMode) {
-      runAutoModeRoutines();
-    }
-
-    // Tick the switches
-    upSwitch.tick();
-    downSwitch.tick();
-    autoSwitch.tick();
-  }
-
-
-  // Run the steppers
-  xstepper.run();
-  loopCnt++;
-}
-
 void moveBlinds(long newPosition) {
   lastMoveAt = millis();
   xstepper.enableOutputs();
@@ -268,23 +356,6 @@ void stopBlinds() {
   xstepper.disableOutputs();
 }
 
-void initializePinIO() {
-  pinMode(DIRECTION, OUTPUT);
-  pinMode(X_STEP_PIN, OUTPUT);
-  pinMode(Y_STEP_PIN, OUTPUT);
-  pinMode(Z_STEP_PIN, OUTPUT);
-  pinMode(A_STEP_PIN, OUTPUT);
-  pinMode(X_STOP_PIN, INPUT_PULLUP);
-  pinMode(Y_STOP_PIN, INPUT_PULLUP);
-  pinMode(Z_STOP_PIN, INPUT_PULLUP);
-  pinMode(A_STOP_PIN, INPUT_PULLUP);
-  pinMode(ENABLE_PIN, OUTPUT);
-  pinMode(SOLENOID, OUTPUT);
-  pinMode(UP_PIN, INPUT_PULLUP);
-  pinMode(DOWN_PIN, INPUT_PULLUP);
-  pinMode(LIGHT, INPUT_PULLUP);
-  pinMode(AUTO_MODE, INPUT_PULLUP);
-}
 
 void runAutoModeRoutines() {
   if (autoModeEnabled() && readyForAutoCmd()) {
@@ -336,34 +407,6 @@ void initializeSteppers() {
   initializeStepper(&xstepper);
 }
 
-void initializeFSMTransitions() {
-  // Transitions in and out of setup
-  fsm.add_transition(&upState, &setupState, AUTO_DOUBLE_CLICK, NULL);
-  fsm.add_transition(&setupState, &downState, AUTO_DOUBLE_CLICK, NULL);
-
-  // Setup up/down control
-  fsm.add_transition(&setupState, &setupMoveUpState, UP_CLICK, NULL);
-  fsm.add_transition(&setupMoveUpState, &setupState, UP_CLICK, NULL);
-  fsm.add_transition(&setupState, &setupMoveDownState, DOWN_CLICK, NULL);
-  fsm.add_transition(&setupMoveDownState, &setupState, DOWN_CLICK, NULL);
-  fsm.add_transition(&setupMoveUpState, &setupState, MOTORS_AT_TOP, NULL);
-
-  // Up to down
-  fsm.add_transition(&upState, &movingDownState, DOWN_CLICK, NULL);
-  fsm.add_transition(&movingDownState, &midwayState, DOWN_CLICK, NULL);
-  fsm.add_transition(&movingDownState, &midwayState, UP_CLICK, NULL);
-  fsm.add_transition(&midwayState, &movingDownState, DOWN_CLICK, NULL);
-  fsm.add_transition(&midwayState, &movingUpState, UP_CLICK, NULL);
-  fsm.add_transition(&movingDownState, &downState, MOTORS_AT_BOTTOM, NULL);
-
-  // Down to up
-  fsm.add_transition(&downState, &movingUpState, UP_CLICK, NULL);
-  fsm.add_transition(&movingUpState, &midwayState, UP_CLICK, NULL);
-  fsm.add_transition(&midwayState, &movingUpState, UP_CLICK, NULL);
-  fsm.add_transition(&movingUpState, &midwayState, DOWN_CLICK, NULL);
-  fsm.add_transition(&midwayState, &movingDownState, DOWN_CLICK, NULL);
-  fsm.add_transition(&movingUpState, &upState, MOTORS_AT_TOP, NULL);
-}
 
 void initializeSwitchHandlers() {
   downSwitch.attachClick([]() {
@@ -385,10 +428,6 @@ void initializeSwitchHandlers() {
     Serial.println("Auto mode engaged.");
     lastMoveAt = millis() - AUTO_TRANSITION_INTERVAL;
   });
-
-  // xStopSwitch.attachPressStart([]() {
-  //  Serial.println("x stop switch engaged");
-  // });
 }
 
 void initializeStepper(AccelStepper *stepper) {
