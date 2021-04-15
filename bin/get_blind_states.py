cb54a15eba5a906df12d02ec2f3ba4f504b92fef
Jamie Gilgen <jamie@gilgen.ca>
2021-04-15 15:34:03 +0100

    Use smbus2

--- bin/get_blind_states.py
+++ bin/get_blind_states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 
-from smbus import SMBus
+from smbus2 import SMBus
 import sys
 import json
 from blind_addresses import blind_address_mappings
 
-# This can be anything not in the command space. For some reason the
-# arduino has fires its receive command handler when requesting a
-# block of data. Anything not in the command space will be ignored
-state_register_address = 200;
+I2C_NOOP = 200
 
-bus = SMBus(1)
+# Since the only data that can be read is the set and current
+# position, we don't have to send a command for the read block
+# data. There's no differing address space we need to specify.
+
 states = {}
 
-for num, addr in blind_address_mappings.items():
-    states[num] = {}
-    try:
-        data = bus.read_i2c_block_data(addr, state_register_address, 2)
-        states[num]['cur'] = int(data[0])
-        states[num]['set'] = int(data[1])
-    except:
-        states[num]['cur'] = 'unreachable'
-        states[num]['set'] = 'unreachable'
+with SMBus(1) as bus:
+    for num, addr in blind_address_mappings.items():
+        states[num] = {}
+        try:
+            data = bus.read_i2c_block_data(addr, I2C_NOOP, 2)
+            states[num]['cur'] = int(data[0])
+            states[num]['set'] = int(data[1])
+        except:
+            states[num]['cur'] = 'unreachable'
+            states[num]['set'] = 'unreachable'
 
 print json.dumps(states)
