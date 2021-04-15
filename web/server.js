2b8ffef991e5a3d2df33237f36680d9f20ea71e6
Jamie Gilgen <jamie@gilgen.ca>
2021-04-15 13:39:20 +0100

    Update express endpoints for bin dir

--- web/server.js
+++ web/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/blinds', (req, res) => {
-  exec(`/usr/bin/python ../get_blind_states.py`, (err, stdout, stderr) => {
+  exec(`/usr/bin/python ../bin/get_blind_states.py`, (err, stdout, stderr) => {
     if (stderr) { console.error(stderr); }
     // console.log("Received blind states: ", stdout);
     res.send(stdout);
@@ -20,7 +20,7 @@ app.put('/api/blinds', (req, res) => {
   console.log(req.body);
   let blinds = req.body.blinds;
   let command = req.body.command;
-  exec(`/usr/bin/python ../send_blind_command.py ${blinds} ${command}`, (err, stdout, stderr) => {
+  exec(`/usr/bin/python ../bin/send_blind_command.py ${blinds} ${command}`, (err, stdout, stderr) => {
     if (stderr) { console.error(stderr); }
     res.send(
       `Update request successfully sent: ${command}`,
