e43f2eb8a02d06b53c6d4a7410b8b1c2d8e45719
Jamie Gilgen <jamie@gilgen.ca>
2021-04-15 03:35:32 +0100

    Starting to get frontend setup

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Slider from '@material-ui/core/Slider';
 
 import logo from './logo.svg';
 
@@ -6,33 +7,57 @@ import './App.css';
 
 class App extends Component {
   state = {
-    response: '',
+    blindStates: '',
     post: '',
     responseToPost: '',
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: JSON.stringify(res) }))
-      .catch(err => console.log(err));
+  blindSets = {
+    "All"        : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+    "East"       : [1,2,3,4,5,6,7,8,9],
+    "North"      : [10,11,12],
+    "West"       : [13,14,15],
+    "North Door" : [10],
+    "West Door"  : [14]
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/blinds');
-    const body = await response.json();
-    console.log("Received body: ", body);
-    if (response.status !== 200) throw Error(body.message);
+  // Poll the arduinos for their state
+  componentDidMount() {
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  refreshBlindStates = async () => {
+    const resp = await fetch('/api/blinds');
+    const body = await resp.json();
+    if (resp.status !== 200) throw Error(body.message);
     return body;
   };
 
+  tick = () => {
+    this.refreshBlindStates().then((blindStatesJson) => {
+      this.setState({ blindStates: JSON.stringify(blindStatesJson) })
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+
+  handleSliderMove(sliderComponent) {
+    console.log("In handle slider move");
+  }
+
   handleSubmit = async e => {
     e.preventDefault();
+    let [blindSetName, command] = this.state.post.split(' ');
+    let blinds = this.blindSets[blindSetName];
+    console.log("blind set name: ", blindSetName);
+    console.log("command: ", command);
+    console.log("blinds: ", blinds);
     const response = await fetch('/api/blinds', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ post: this.state.post }),
+      body: JSON.stringify({ blinds: blinds, command: command }),
     });
 
     const body = await response.text();
@@ -40,26 +65,42 @@ class App extends Component {
     this.setState({ responseToPost: body });
   };
 
+  handleChange(event) {
+    console.log("handle change");
+  }
+
   render() {
-    let responseToGet = this.state.response;
-    if (!responseToGet) {
-      responseToGet = "Loading data from backend...";
+    let blindStates = this.state.blindStates;
+    let curVal = 100;
+    if (!blindStates) {
+      blindStates = "Loading blind states...";
+    } else {
+      curVal = blindStates["2"]["cur"];
     }
+    let style = { height: "300px" };
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <p>{responseToGet}</p>
+        <p>{blindStates}</p>
+
+        <Slider
+          orientation="vertical"
+          style={style}
+          value={curVal}
+          defaultValue={100}
+          onChange={this.handleChange}
+          min={0}
+          max={100}
+        />
+
         <form onSubmit={this.handleSubmit}>
           <p>
             <strong>Post to Server:</strong>
           </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
+          <input type="text" value={this.state.post} onChange={e => this.setState({ post: e.target.value })} />
           <button type="submit">Submit</button>
         </form>
         <p>{this.state.responseToPost}</p>
--- web/server.js
+++ web/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const i2c = require('i2c-bus');
 const { exec } = require("child_process");
-// const I2C_BUS = 1;
-// const ADDR = 0B10010;
 
 const app = express();
 const port = 5000;
@@ -11,27 +8,22 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
 app.get('/api/blinds', (req, res) => {
   exec(`/usr/bin/python ../get_blind_states.py`, (err, stdout, stderr) => {
     if (stderr) { console.error(stderr); }
-    // stdout is expected to be JSON in the format:
-    // { 1: { set: 60, cur: 20 }, 2: { set: 50, cur: 30 }, ...
-    console.log(stdout);
+    // console.log("Received blind states: ", stdout);
     res.send(stdout);
   });
 });
 
 app.put('/api/blinds', (req, res) => {
   console.log(req.body);
-  let command = req.body.post;
-  exec(`/usr/bin/python ../send_cmd.py ${command}`, (err, stdout, stderr) => {
+  let blinds = req.body.blinds;
+  let command = req.body.command;
+  exec(`/usr/bin/python ../send_blind_command.py ${blinds} ${command}`, (err, stdout, stderr) => {
     if (stderr) { console.error(stderr); }
     res.send(
-      `I received your POST request. This is what I set the blinds to: ${command}`,
+      `Update request successfully sent: ${command}`,
     );
   });
 });
