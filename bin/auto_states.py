00c5ad59889bac1b2c4648a0285c898f003b1783
Jamie Gilgen <jamie@gilgen.ca>
2021-04-23 16:52:28 +0100

    Use some better variable names in blind i2c command handlers

--- bin/auto_states.py
+++ bin/auto_states.py
@@ -4,6 +4,7 @@ import json
 
 AUTO_STATES_FILE = os.path.join(sys.path[0], '.auto-states')
 
+# this is a convenience json to dict wrapper around the .auto-states file
 with open(AUTO_STATES_FILE) as f:
   auto_states = json.load(f)
 
--- bin/send_blind_command.py
+++ bin/send_blind_command.py
@@ -14,6 +14,7 @@ from auto_sensors import auto_sensors
 # Example: Move the blinds 1,4,6, and 7 to 50% down
 # /usr/bin/python send_cmd.py 1,4,6,7 50
 
+
 command = int(sys.argv[2])
 blinds = list(map(int, sys.argv[1].split(',')))
 blind_to_sensor = {}
@@ -21,26 +22,24 @@ blind_to_sensor = {}
 # Find the auto sensor number for the blind being acted on
 # so we can update the lastCommandAt
 for blind_num in blinds:
-    print "Searching for sensor associated with: {}".format(blind_num)
     for sensor, sensor_data in auto_sensors.items():
-        print "Looking at sensor {}, {}".format(sensor, sensor_data)
+        # print "Looking at sensor {}, {}".format(sensor, sensor_data)
         if blind_num in sensor_data['blindNums']:
-            print "Found blind num {} in sensor {}".format(blind_num, sensor)
+            # print "Found blind num {} in sensor {}".format(blind_num, sensor)
             blind_to_sensor[blind_num] = str(sensor)
             auto_sensor_num = str(sensor)
-            continue
+            break
 
 print blind_to_sensor
 
 with SMBus(1) as bus:
-    for i in blinds:
+    for blind in blinds:
         try:
-            bus.write_byte(blind_address_mappings[i], command)
-            print('Sent {} to blind {}'.format(command, i))
-            auto_states[blind_to_sensor[i]]['lastCommandAt'] = int(time.time())
+            auto_states[blind_to_sensor[blind]]['lastCommandAt'] = int(time.time())
+            bus.write_byte(blind_address_mappings[blind], command)
+            print('Sent {} to blind {}'.format(command, blind))
         except IOError:
-            print('Blind {} unreachable'.format(i))
-            continue
+            print('Blind {} unreachable'.format(blind))
 
 # If an auto sensor number was passed in, we have to write the
 # updated auto states file with the new last command at values
